16c755f9b18ea868c921039bb109a000e29191f0
Ludwig <ludwig.slettingdalen@gmail.com>
2022-12-12 20:38:00 +0100

    Feat (#2)
    * importing the functions are working
    
    * functionality ok
    
    * Commit før refactoring av funsjoner
    
    * new services.ts and downlevelIteration: true, because of isOnePair function
    
    * changed to norwegian lowecase letters
    
    * knekt, k ble til kn på grunn av k for kløver
    
    * added card images
    
    * deleted some console logs
    
    * Cards are displaying on the site
    
    * card images displaying correct
    
    * changed knekt to c and added isValidHand loop
    
    * commiting before trying some changes
    
    * commit before some jest
    
    * Feat jest (#1)
    
    * lagt noen tester
    
    * renamed getNum to convertToNumbers
    
    * commit
    
    * commit
    
    * convertert isOnePair, isTwoPair og isThreeOfAKind til numArray
    
    * Added some more tests
    
    * converted all functions except isFlush tu numRanks
    
    * camelCase in isTwoPair
    
    * commit
    
    * refactoring
    
    * hydration fix
    
    * readme

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
+import { PokerHand } from "./components/pokerHand"
+
 export default function Home() {
   return (
     <div>
-      <h1 className="text-3xl font-bold text-center m-10" >Hello World!</h1>
+      <PokerHand />
     </div>
   )
 }
