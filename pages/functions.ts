2dc58234117c31e1c4d22b97511e8f12d16fdb55
Ludwig <ludwig.slettingdalen@gmail.com>
2022-12-12 17:33:56 +0100

    commit

--- pages/functions.ts
+++ pages/functions.ts
@@ -1,10 +1,13 @@
 
+// TODO
+// Fikse Any
+// Gjøre om let til const
 
 const tallverdi = ["2", "3", "4", "5", "6", "7", "8", "9", "t", "c", "d", "k", "e"]
 const kortfarge = ["k", "r", "h", "s"]
 
 export const isFourOfAKind = (ranks: string[]) => { // Her måtte konvertert ranks til numbers helt i starten, endre på mye greier...
-  const num = convertToNumbers(ranks) // 
+  const num = convertToNumbers(ranks) // Få alle disse inn i getPokerHandType funksjonen?
     .sort((a, b) => a - b)
   const fourFirst = num[0] === num[1] && num[1] === num[2] && num[2] === num[3]
   const fourLast = num[1] === num[2] && num[2] === num[3] && num[3] === num[4]
@@ -12,7 +15,7 @@ export const isFourOfAKind = (ranks: string[]) => { // Her måtte konvertert ran
 }
 
 export const isFullHouse = (ranks: string[]) => {
-  let array = convertToNumbers(ranks)
+  let array = convertToNumbers(ranks) // Få alle disse inn i getPokerHandType funksjonen?
   array.sort((a, b) => a - b)
   const fullHouseLowPair = array[0] === array[1] && array[2] === array[3] && array[3] === array[4]
   const fullHouseHighPair = array[0] === array[1] && array[1] === array[2] && array[3] === array[4]
@@ -49,16 +52,40 @@ export const isTwoPair = (ranks: any[]) => { // Her måtte konvertert ranks til 
 
 export const isOnePair = (ranks: string[]) => [...new Set(ranks)].length === 4
 
+
+
 // Helper that converts the ranks to numbers
-const convertToNumbers = (ranks: string[]) => {
+// const convertToNumbers = (ranks: string[]) => {
+//   return ranks.map((element) => {
+//     if (element === 't') return 10
+//     if (element === 'c') return 11
+//     if (element === 'd') return 12
+//     if (element === 'k') return 13
+//     if (element === 'e') return 14
+//     else return Number(element)
+//   })
+// }
+
+
+
+// Helper that converts the ranks to numbers
+const convertToNumbers = (ranks: any[]) => {
   return ranks.map((element) => {
-    if (element === 't') return 10
-    if (element === 'c') return 11
-    if (element === 'd') return 12
-    if (element === 'k') return 13
-    if (element === 'e') return 14
-    else return Number(element)
-  })
+      switch (element) {
+          case 't':
+              return 10;
+          case 'c':
+              return 11;
+          case 'd':
+              return 12;
+          case 'k':
+              return 13;
+          case 'e':
+              return 14;
+          default:
+              return Number(element);
+      }
+  });
 }
 
 export const generatePokerHand = () => {
@@ -106,3 +133,32 @@ export const getAnalysedPokerHand = () => {
   return { hand, handType }
 }
 
+
+
+// Se om jeg får imlementert dette?
+
+// const generateDeck = () => { // map a map and flatten it down to a single array?
+//   const suits = ["c", "d", "h", "s"]
+//   const values = ["2", "3", "4", "5", "6", "7", "8", "9", "t", "j", "d", "k", "e"]
+//   const deck = []
+//   suits.forEach(suit => {
+//       values.forEach(value => {
+//           deck.push(value + suit)
+//       })
+//   })
+//   return deck
+// }
+
+// const shuffleDeck = (deck) => {
+//   deck.map((card, index) => {
+//       const randomIndex = Math.floor(Math.random() * deck.length)
+//       const temp = deck[index] // can be card
+//       deck[index] = deck[randomIndex] // can be card?
+//       deck[randomIndex] = temp
+//   })
+//   return deck
+// }
+
+// const getPokerHand = (shuffledDeck) => { return shuffledDeck.slice(0, 5) }
+
+// const pokerHand = getPokerHand(shuffleDeck(generateDeck()))
